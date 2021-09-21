4bd6097c2e5314d2fcd998207b6a53600f9ac635
marstefano <stefano.2020@smk-strada.sch.id>
2021-09-21 09:18:22 +0700

    nama tugas stefano

--- jawaban_1.js
+++ jawaban_1.js
@@ -1,1 +1,26 @@
 // Jawaban Soal 1 ada di bawah ini
+class Hewan {
+    constructor(nama, warna, beratGram, harga) {
+        this.nama = nama;
+        this.warna = warna;
+        this.beratGram = beratGram;
+        this.harga = harga;
+        this.kondisiBagus = false;
+    }
+
+    cekKondisiBagus(kondisi = false) {
+        return this.kondisiBagus = kondisi;
+    }
+}
+
+// Cara membuat object instance
+
+let hewanKucing = new Hewan("Anjing", "Hitam", 25, 1000000);
+hewanKucing.cekKondisiBagus(true);
+console.log(hewanAnjing);
+let hewanBebek = new Hewan("Kelinci", "Putih", 25)
+hewanKelinci.cekKondisiBagus(true);
+console.log(hewanKelinci);
+
+console.log('ini hewan Anjing =>', hewanAnjing);
+console.log('ini hewan Kelimci =>', hewanKelinci);
